03b90f40eb0921c245e3550e89976e549f9091d9
CodersBros <p.grden@codersbros.pl>
2025-10-02 11:55:00 +0200

    feat: Rearrange sections in Home component for improved structure and readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,35 @@
-import HeroSection from '@/components/ui/HeroSection';
 import HeroIconAction from '@/components/ui/HeroIconAction';
+import HeroSection from '@/components/ui/HeroSection';
 import OurServicesSection from '@/components/ui/OurServicesSection';
 import { SERVICES_ITEMS } from '@/data/services';
+import CaseStudySection from '../components/ui/CaseStudySection';
+import TeamFilterSection from '../components/ui/TeamFilterSection';
 
 export default function Home() {
   return (
     <div>
-      <HeroSection
-        backgroundImage={'/images/hero_background.jpg'}
-        label={
-          'Control Your Music. Empower Your Instructors. Engage Every Facility.'
-        }
-        actions={
-          <HeroIconAction
-            label="Music & voice for gyms"
-          />
-        }
-      />
-      <OurServicesSection items={SERVICES_ITEMS} />
+      <section id="hero">
+        <HeroSection
+          backgroundImage={'/images/hero_background.jpg'}
+          label={
+            'Control Your Music. Empower Your Instructors. Engage Every Facility.'
+          }
+          actions={
+            <HeroIconAction
+              label="Music & voice for gyms"
+            />
+          }
+        />
+      </section>
+      <section id="services">
+        <OurServicesSection items={SERVICES_ITEMS} />
+      </section>
+      <section id="case-studies">
+        <CaseStudySection />
+      </section>
+      <section id="team">
+        <TeamFilterSection />
+      </section>
     </div>
   );
 }
