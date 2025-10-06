8c882ee52a7b1bd091abf99d22e349fdb2a4d7c6
CodersBros <p.grden@codersbros.pl>
2025-10-06 23:59:33 +0200

    refactor: Replace section tags with Wrapper components for consistent styling and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,48 @@
+'use client';
+
 import HeroIconAction from '@/components/ui/HeroIconAction';
 import HeroSection from '@/components/ui/HeroSection';
 import OurServicesSection from '@/components/ui/OurServicesSection';
 import { SERVICES_ITEMS } from '@/data/services';
 import CaseStudySection from '../components/ui/CaseStudySection';
+import OurDevelopmentSections from '../components/ui/OurDevelopmentSections';
 import TeamFilterSection from '../components/ui/TeamFilterSection';
+import { Wrapper } from '../styles/index.styled';
 
 export default function Home() {
   return (
     <div>
-      <section id="hero">
+      <Wrapper as='section' id='hero'>
         <HeroSection
           backgroundImage={'/images/hero_background.jpg'}
           label={
             'Control Your Music. Empower Your Instructors. Engage Every Facility.'
           }
-          actions={
-            <HeroIconAction
-              label="Music & voice for gyms"
-            />
-          }
+          actions={<HeroIconAction label='Music & voice for gyms' />}
         />
-      </section>
-      <section id="services">
+        <Wrapper
+          as='section'
+          $desktopPadding='100px 0 0 0'
+          $mobilePadding='84px 0 0 0'
+        >
+          <OurDevelopmentSections />
+        </Wrapper>
+      </Wrapper>
+      <Wrapper as='section' $mobilePadding='120px 0 0 0' id='services'>
         <OurServicesSection items={SERVICES_ITEMS} />
-      </section>
-      <section id="case-studies">
+      </Wrapper>
+      <Wrapper as='section' id='case-studies' $mobilePadding='120px 0 0 0'>
         <CaseStudySection />
-      </section>
-      <section id="team">
+      </Wrapper>
+      <Wrapper
+        as='section'
+        id='team'
+        $mobilePadding='120px 0 90px 0'
+        $tabletPadding='120px 0 30px 0'
+        $tabletXLPadding='120px 0 160px 0'
+      >
         <TeamFilterSection />
-      </section>
+      </Wrapper>
     </div>
   );
 }
