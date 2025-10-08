bc289a2f3e5cebd8df0de3be804943f93a0d9058
CodersBros <p.grden@codersbros.pl>
2025-10-08 09:11:08 +0200

    refactor: Replace Home component with HomeClient for improved structure and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,11 @@
-'use client';
+import type { Metadata } from 'next';
+import HomeClient from './HomeClient';
 
-import HeroIconAction from '@/components/ui/HeroIconAction';
-import HeroSection from '@/components/ui/HeroSection';
-import OurServicesSection from '@/components/ui/OurServicesSection';
-import { SERVICES_ITEMS } from '@/data/services';
-import CaseStudySection from '../components/ui/CaseStudySection';
-import OurDevelopmentSections from '../components/ui/OurDevelopmentSections';
-import TeamFilterSection from '../components/ui/TeamFilterSection';
-import { Wrapper } from '../styles/index.styled';
+export const metadata: Metadata = {
+  title: 'Streamec – Music & Voice Platform for Gyms, Trainers & Studios',
+  description: 'Streamec empowers fitness clubs, instructors, and wellness studios with synchronized music and voice streaming. Create perfect workouts with professional sound and real-time control.',
+};
 
-export default function Home() {
-  return (
-    <div>
-      <Wrapper as='section' id='hero'>
-        <HeroSection
-          backgroundImage={'/images/hero_background.jpg'}
-          label={
-            'Control Your Music. Empower Your Instructors. Engage Every Facility.'
-          }
-          actions={<HeroIconAction label='Music & voice for gyms' />}
-        />
-        <Wrapper
-          as='section'
-          $desktopPadding='100px 0 0 0'
-          $mobilePadding='84px 0 0 0'
-        >
-          <OurDevelopmentSections />
-        </Wrapper>
-      </Wrapper>
-      <Wrapper as='section' $mobilePadding='120px 0 0 0' id='services'>
-        <OurServicesSection items={SERVICES_ITEMS} />
-      </Wrapper>
-      <Wrapper as='section' id='case-studies' $mobilePadding='120px 0 0 0'>
-        <CaseStudySection />
-      </Wrapper>
-      <Wrapper
-        as='section'
-        id='team'
-        $mobilePadding='120px 0 90px 0'
-        $tabletPadding='120px 0 30px 0'
-        $tabletXLPadding='120px 0 160px 0'
-      >
-        <TeamFilterSection />
-      </Wrapper>
-    </div>
-  );
+export default function HomePage() {
+  return <HomeClient />;
 }
